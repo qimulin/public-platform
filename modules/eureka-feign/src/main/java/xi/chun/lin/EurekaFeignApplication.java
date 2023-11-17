42972cf78474c158a60f8c0ac66084b50085f266
linxc <582192258@qq.com>
2023-11-17 11:26:15 +0800

    依赖规范设置，解决maven clean install报错问题

--- modules/eureka-feign/src/main/java/xi/chun/lin/EurekaFeignApplication.java
+++ modules/eureka-feign/src/main/java/xi/chun/lin/EurekaFeignApplication.java
@@ -5,10 +5,8 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 应用主类中通过@EnableFeignClients注解开启Feign功能
